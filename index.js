947702d72b1dd870ef1b3802078a05c1a72cb8bd
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-07-14 19:16:02 +0530

    checnged port

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static(path.join(__dirname,'gitAPI.js')));
 app.use('/',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'gitAPI.html'));
 });
+
+app.set(8080, process.env.PORT || 3000);
+
 app.listen(8080,()=>{
     console.log('Server is running on port 8080');
 });
