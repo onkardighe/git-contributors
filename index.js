31be3db1c04e585016ede93dfeef4023598b3cb0
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-07-15 22:57:04 +0530

    Updating index.js port no

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const app = express();
 const path = require('path');
 
 
-
 app.use(express.static(path.join(__dirname,'/assets')));
 app.use(express.static(path.join(__dirname,'gitAPI.js')));
 
 
-app.use('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'gitAPI.html'));
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.set(8080, process.env.PORT || 3000);
 
-app.listen(8080,()=>{
+app.listen(process.env.PORT || 8080,()=>{
     console.log('Server is running on port 8080');
 });
 
